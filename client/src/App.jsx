eb98a32f6d6ed94841c8062ef8a01e6b3186fa7c
David Boyea <dboyea@cimlbr.com>
2021-06-30 13:17:34 -0500

    Implements bootstrap 4 style syntax

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,12 +7,17 @@ class App extends React.Component {
   render() {
       return (
         <div>
+            <div class="card">
+              <div class="card-body">
                 <div class="row">
+                  <div class="col-4"></div>
                   <div class="col-4">
                     <h1>Hello General Sarai</h1>
                   </div>
                   <div class="col-4"></div>
                 </div>
+              </div>
+            </div>
         </div>
       )
   }
